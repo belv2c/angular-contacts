e4e17696561c8310dd1ba166c6ab2ffb6479c519
Brooke Lewis <belv2c@gmail.com>
2017-11-26 14:51:31 -0600

    working through edit functionality

--- javascripts/controllers/FavoritesCtrl.js
+++ javascripts/controllers/FavoritesCtrl.js
@@ -1,12 +1,33 @@
 "use strict";
 
-app.controller("FavoritesCtrl", function($rootScope, $scope, ContactService) {
+app.controller("FavoritesCtrl", function($location, $rootScope, $scope, ContactService) {
 	const getContacts = () => {
 		ContactService.getFavorites($rootScope.uid).then((results) => {
 			$scope.contacts = results;
 		}).catch((err) => {
 			console.log("error in getContacts", err);
 		});
+		
 	};
-	getContacts();
+
+
+$scope.deleteContact = (userId) => {
+		ContactService.deleteContact(userId).then((results) => {
+			getContacts();
+		}).catch((err) => {
+			console.log("error in deleteContact", err);
+		});
+	};
+
+
+$scope.editContact = (userId) => {
+	$location.path(`/contacts/edit/${userId}`);
+};
+
+$scope.contactDetail = (userId) => {
+	$location.path(`/contacts/detail/${userId}`);
+};
+
+getContacts();
+
 });
