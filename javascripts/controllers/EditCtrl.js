900ba7e062dd316277349d49d02b6ba7bf685e40
Brooke Lewis <belv2c@gmail.com>
2017-11-26 14:15:43 -0600

    added edit functionality

--- javascripts/controllers/EditCtrl.js
+++ javascripts/controllers/EditCtrl.js
@@ -1,1 +1,33 @@
 "use strict";
+
+app.controller("EditCtrl", function($location, $rootScope, $routeParams, $scope, ContactService) {
+	
+const getContacts = () => {
+		ContactService.getOneContact($routeParams.id).then((results) => {
+			$scope.contacts = results;
+		}).catch((err) => {
+			console.log("error in getContacts", err);
+		});
+	};
+
+	$scope.submitForm = () => {
+		let updatedContact = {
+			"firstName": $scope.newcontact.firstname,
+			"lastName": $scope.newcontact.lastname,
+			"address": $scope.newcontact.address,
+			"phoneNumber": $scope.newcontact.phonenumber,
+			"email": $scope.newcontact.email,
+			"birthday": $scope.newcontact.birthday,
+			"nickName": $scope.newcontact.nickname,
+			"user_id": $rootScope.uid,
+			"favorite": false
+		};
+		
+		ContactService.updateContact(updatedContact, $routeParams.id).then((results) => {
+			$location.path("/contacts/view");
+		}).catch((err) => {
+			console.log("error in submitForm", err);
+		});
+	};
+	getContacts();
+});
